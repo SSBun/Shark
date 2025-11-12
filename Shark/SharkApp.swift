5110b575dcb8c1abb965ac708dabdbc0d1825b90
CSL <caishilin@zhihu.com>
2025-11-12 10:20:05 +0800

    Initial Commit

---
+++ Shark/SharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharkApp.swift
+//  Shark
+//
+//  Created by caishilin on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct SharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
